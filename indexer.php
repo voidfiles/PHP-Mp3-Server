f73833e2964ac78d9236013787c5974acf79789b
Alexander Kessinger <voidfiles@gmail.com>
2009-08-29 00:52:27 -0700

    added an indexer

---
+++ indexer.php
@@ -0,0 +1,33 @@
+<?PHP
+
+
+function listFiles( $from = '.')
+{
+    if(! is_dir($from))
+        return false;
+   
+    $files = array();
+    $dirs = array( $from);
+    while( NULL !== ($dir = array_pop( $dirs)))
+    {
+        if( $dh = opendir($dir))
+        {
+            while( false !== ($file = readdir($dh)))
+            {
+                if( $file == '.' || $file == '..')
+                    continue;
+                $path = $dir . '/' . $file;
+                if( is_dir($path))
+                    $dirs[] = $path;
+                else
+                    $files[] = $path;
+				print $path;
+            }
+            closedir($dh);
+        }
+    }
+    return $files;
+}
+
+print_r(listFiles());
+?>
